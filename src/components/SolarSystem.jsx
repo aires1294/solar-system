1587af6652fa9e6f6542e80b4c6fdc0aa5cc895e
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-18 17:46:09 -0300

    item 6 feitoo

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planetObj) => (
+          <PlanetCard
+            className="planetas"
+            key={ planetObj.name }
+            planetName={ planetObj.name }
+            planetImage={ planetObj.image }
+          />
+        ))}
       </div>
     );
   }
