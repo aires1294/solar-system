c0fe3c3120d4dbf161bc97f794a6f90314662f6a
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-18 17:19:27 -0300

    item 5 FEITOO

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
+    // console.log(headline);
     return <h2>{ headline }</h2>;
   }
 }
