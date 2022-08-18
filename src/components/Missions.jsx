d6b10c8ee9edb292be523e11fe1a93ca39331ceb
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-18 18:19:58 -0300

    Item 8 feitooo

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-
-class Missions extends React.Component {
-  render() {
-    return <div data-testid="missions" />;
-  }
-}
-
-export default Missions;
-
 // import React from 'react';
 
 // class Missions extends React.Component {
 //   render() {
-//     return (
-//       <div>
-//         <div data-testid='missions' />
-//         <h1>TESTEEEEEEEEEE</h1>
-//       </div>
-//     );
+//     return <div data-testid="missions" />;
 //   }
 // }
 
 // export default Missions;
+
+import React from 'react';
+import Title from './Title';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div>
+        <div data-testid="missions" />
+        <Title headline="Missões" />
+      </div>
+    );
+  }
+}
+
+export default Missions;
