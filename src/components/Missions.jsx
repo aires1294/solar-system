6f23b29d5127c7d3ac5836964d62ae9e2361d371
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-18 18:52:09 -0300

    Item 10 feitooo

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -10,6 +10,8 @@
 
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
@@ -17,6 +19,15 @@ class Missions extends React.Component {
       <div>
         <div data-testid="missions" />
         <Title headline="Missões" />
+        {missions.map((missaoObj) => (
+          <MissionCard
+            key={ missaoObj.name }
+            name={ missaoObj.name }
+            year={ missaoObj.year }
+            country={ missaoObj.country }
+            destination={ missaoObj.destination }
+          />
+        ))}
       </div>
     );
   }
