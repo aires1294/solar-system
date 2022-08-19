6c3eeeebd88ac192071e1b070f2d65832510195a
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-19 09:56:13 -0300

    corrigindo erros de PropTypes e lint

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
@@ -15,8 +16,8 @@ class MissionCard extends React.Component {
 }
 
 MissionCard.propTypes = {
-  name: PropTypes.stryng.isRequired,
-  year: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
 };
